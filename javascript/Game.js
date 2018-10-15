77ba9154b70ea220e138f9b73930608ce815b2ea
elly torres <nallelyt4@gmail.com>
2018-10-14 20:11:58 -0500

    Add function for retrieving questions from randomly generated categories

--- javascript/Game.js
+++ javascript/Game.js
@@ -1,13 +1,11 @@
 class Game {
   constructor(round, players, winner) {
-    this.round = 0,
-    this.players = [],
+    this.roundNumber = 0,
+    this.players = players,
     this.winner = winner,
     this.categories = []
+    this.round = new Round();
   }
-
-
-
 
   start() {
     this.createCategories()
@@ -21,24 +19,26 @@ class Game {
 
   declareWinner() {
   }
-
-  createCategories() {
-    let categoryKeys = Object.entries(data.categories);
-      let randoms = [];
-        for (var i = 0; i < categoryKeys.length; i++) {
-
-          let rand = categoryKeys[Math.floor(Math.random() * categoryKeys.length)]
-            if (randoms.indexOf(rand) === -1) {
-              randoms.push(rand)
-            }  if (randoms.length > 4) {
-              randoms.pop()
-            }
-          }
-      return this.categories.push(randoms);
-    }
+  startNewRound() {
+    this.roundNumber++;
+    this.round = new Round();
   }
 
+createCategories() {
+  let categoryKeys = Object.entries(data.categories);
+    let randoms = [];
+      for (var i = 0; i < categoryKeys.length; i++) {
 
+        let rand = categoryKeys[Math.floor(Math.random() * categoryKeys.length)]
+          if (randoms.indexOf(rand) === -1) {
+            randoms.push(rand)
+          }  if (randoms.length > 4) {
+            randoms.pop()
+          }
+        }
+    return this.categories.push(randoms);
+  }
+}
 
 
 if (typeof module !== 'undefined') {
--- javascript/Round.js
+++ javascript/Round.js
@@ -1,42 +1,70 @@
 class Round {
   constructor() {
-    this.turn = 1
-    this.questions = []
-    this.askedQuestions = []
-    this.roundOver = false
-    this.dailyDoubles = []
-    this.data = data
+    this.turn = 1;
+    this.askedQuestions = [];
+    this.roundOver = false;
+    this.dailyDoubles = [];
+    this.data = data;
+    this.createRound();
   }
 
 // createRound();
 
-  createRound() {
-    const categories = this.createCategories();
-    const categoryIds = this.getCategoryIds(categories);
-  }
+createRound() {
+  this.categories = this.createCategories();
 
+  const categoryIds = this.getCategoryIds(categories);
+  this.questions = this.getQuestions(this.categories)
+}
 
-  createCategories() {
-    let categoryKeys = Object.keys(this.data.categories);
-    let randoms = [];
-      for (var i = 0; i < categoryKeys.length; i++) {
-
-        let rand = categoryKeys[Math.floor(Math.random() 
-                    * categoryKeys.length)]
-          if (randoms.indexOf(rand) === -1) {
-            randoms.push(rand)
-          }  if (randoms.length > 4) {
-            randoms.pop()
-          }
+createCategories() {
+  let categoryKeys = Object.keys(this.data.categories);
+  let randoms = [];
+    for (var i = 0; i < categoryKeys.length; i++) {
+      let rand = categoryKeys[Math.floor(Math.random() * categoryKeys.length)];
+        if (randoms.indexOf(rand) === -1) {
+          randoms.push(rand)
+        } if (randoms.length > 4) {
+          randoms.pop()
         }
-    
-    return randoms;
+      }
+  
+  return randoms;
+}
+
+getQuestions(categories) {
+  let categories = data.clues.reduce((obj, item) => {
+  if(randoms.indexOf(item.categoryId) > -1) {
+    if(obj[item.categoryId] == null)  
+    obj[item.categoryId] = [];
+    obj[item.categoryId].push(item);
   }
+  return obj;
+  }, {});
 
-  getCategoryIds(categories) {
-    return categories.map((category) => {
-      return this.data.categories[category]
-    })
+  let categoryQuestions = {};
+
+  Object.keys(categories).forEach((catKey) => {
+  const cat = categories[catKey];
+  let current = 100;
+
+  const bunches = cat.reduce((acc, data) => {
+    if (data.pointValue === current) {
+      current = current + 100
+      acc.push(data)
+    }
+    return acc;
+  }, [])
+
+  if(!categoryQuestions[catKey]) categoryQuestions[catKey] = {};
+  categoryQuestions[catKey].questions = bunches
+  });
 }
 
-}
+// getCategoryIds(categories) {
+//   return categories.map((category) => {
+//     return this.data.categories[category]
+//   });
+// }
+
+// }
