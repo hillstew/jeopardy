011c4336a32f216c791a5641b732dc198cd3dcab
Hillary Stewart <hillarystewart4427@gmail.com>
2018-10-18 11:19:07 -0600

    Remove hover from selected question

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,6 +12,9 @@ $('.categories').on('click', (e) => {
   let category = $(e.target).siblings('.category-cards').text();
   let pointValue = parseInt($(event.target).text());
   game.matchQuestion(category, pointValue);
+  $(e.target).text('X');
+  $(e.target).removeClass('question-cards')
+  $(e.target).addClass('question-no-hover')
 });
 
 $('.answer-btn').on('click', () => {
